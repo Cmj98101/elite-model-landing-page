4443a7b91a4c91ee8d9d99220312f122c8b30416
Christopher <Christopher.Jtech@gmail.com>
2021-08-13 09:11:10 -0700

    async await added

--- app.js
+++ app.js
@@ -6,31 +6,34 @@ const exphbs = require('express-handlebars');
 const path = require('path');
 const nodemailer = require("nodemailer");
 
-dotenv.config();
+
+async function main() {
+
+    dotenv.config();
 
 
-const app = express();
+    const app = express();
 
-// View engine setup
-app.engine('handlebars', exphbs());
-app.set('view engine', 'handlebars');
+    // View engine setup
+    app.engine('handlebars', exphbs());
+    app.set('view engine', 'handlebars');
 
-// Static folder
-app.use('/public', express.static(path.join(__dirname, 'public')));
-// Body Parser Middleware
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
-app.use(bodyParser.json());
+    // Static folder
+    app.use('/public', express.static(path.join(__dirname, 'public')));
+    // Body Parser Middleware
+    app.use(bodyParser.urlencoded({
+        extended: false
+    }));
+    app.use(bodyParser.json());
 
 
-app.get('/', function (req, res) {
-    res.render('home', {
-        layout: false
+    app.get('/', function (req, res) {
+        res.render('home', {
+            layout: false
+        });
     });
-});
-app.post('/send', (req, res) => {
-    const output = `
+    app.post('/send', async (req, res) => {
+        const output = `
     <p>You have a new contact request</p
     <h3>Contact Details</h3>
     <ul>
@@ -41,41 +44,44 @@ app.post('/send', (req, res) => {
     <h3>Question:</h3>
     <p>${req.body.questions}</p>
     `;
-    // create reusable transporter object using the default SMTP transport
-    let transporter = nodemailer.createTransport({
-        host: "smtp.sendgrid.net",
-        port: 465,
-        secure: true, // true for 465, false for other ports
-        auth: {
-            user: 'apikey', // generated ethereal user
-            pass: `${process.env.API_KEY}`, // generated ethereal password
-        },
-        tls: {
-            rejectUnauthorized: false
-        }
-    });
+        // create reusable transporter object using the default SMTP transport
+        let transporter = nodemailer.createTransport({
+            host: "smtp.sendgrid.net",
+            port: 465,
+            secure: true, // true for 465, false for other ports
+            auth: {
+                user: 'apikey', // generated ethereal user
+                pass: `${process.env.API_KEY}`, // generated ethereal password
+            },
+            tls: {
+                rejectUnauthorized: false
+            }
+        });
 
-    // send mail with defined transport object sean@velocityplotter.com, janier@velocityplotters.com
-    let info = transporter.sendMail({
-        from: '"Submission Form" <chris@velocityplotters.com>', // sender address
-        to: "chris@velocityplotters.com", // list of receivers
-        subject: "Landing Page Submission Form", // Subject line
-        text: "Hello world?", // plain text body
-        html: output, // html body
-    }, (error, info) => {
+        // send mail with defined transport object sean@velocityplotter.com, janier@velocityplotters.com
+        let info = await transporter.sendMail({
+            from: '"Submission Form" <chris@velocityplotters.com>', // sender address
+            to: "chris@velocityplotters.com", // list of receivers
+            subject: "Landing Page Submission Form", // Subject line
+            text: "Hello world?", // plain text body
+            html: output, // html body
+        });
         console.log("Message sent: %s", info.messageId);
 
-        console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+        // console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
         res.render('home', {
             msg: "Email has been sent!",
             layout: false,
         });
+
+
+
+
     });
+    const server = app.listen(process.env.PORT || 3000, () => {
+        const port = server.address().port;
+        console.log(`Express is working on port ${port}`);
 
-
-
-});
-const server = app.listen(process.env.PORT || 3000, () => {
-    const port = server.address().port;
-    console.log(`Express is working on port ${port}`);
-});
+    });
+}
+main().catch(console.error);
